2d7e8511427b66ff3dcf60b1e3fce2035eb2d90d
dell <dell@DESKTOP-4P8U8AR>
2024-04-25 23:09:01 +0100

    Creating Type Model

---
+++ src/main/java/com/ilyass/activity/models/Type.java
@@ -0,0 +1,32 @@
+package com.ilyass.activity.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "types")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String typeName;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+
+}
