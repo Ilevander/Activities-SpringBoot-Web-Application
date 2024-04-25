a1627cea14764537d8411747570bd670e51ae9d9
dell <dell@DESKTOP-4P8U8AR>
2024-04-25 23:27:27 +0100

    Creating Status Model

---
+++ src/main/java/com/ilyass/activity/models/Status.java
@@ -0,0 +1,32 @@
+package com.ilyass.activity.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "statuses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String statusName;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+}
