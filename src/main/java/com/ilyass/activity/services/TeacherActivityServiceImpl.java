5f1f3587a6f1c923de2cc278357f7a0f148d3bf0
dell <dell@DESKTOP-4P8U8AR>
2024-05-07 18:46:16 +0100

    Implementation of Teacher Activity Service

---
+++ src/main/java/com/ilyass/activity/services/TeacherActivityServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ilyass.activity.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ilyass.activity.models.Teacher;
+import com.ilyass.activity.models.Activity;
+
+@Service
+public class TeacherActivityServiceImpl implements TeacherActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    @Autowired
+    public TeacherActivityServiceImpl(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    @Override
+    public List<Activity> getAllActivitiesForTeacher(Teacher teacher) {
+        return activityRepository.findAllByTeacher(teacher);
+    }
+
+    @Override
+    public Activity createActivity(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    @Override
+    public Activity updateActivity(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    @Override
+    public void deleteActivity(Long activityId) {
+        activityRepository.deleteById(activityId);
+    }
+}
