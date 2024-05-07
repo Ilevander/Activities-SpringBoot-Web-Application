562a1236f8b44d87e8e197a9d0b9f64c96895613
dell <dell@DESKTOP-4P8U8AR>
2024-05-07 19:26:37 +0100

    Customizing Activity Repository

--- src/main/java/com/ilyass/activity/repository/ActivityRepository.java
+++ src/main/java/com/ilyass/activity/repository/ActivityRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.ilyass.activity.models.Activity;
 import com.ilyass.activity.models.Teacher;
-
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
@@ -14,7 +14,14 @@ public interface ActivityRepository extends JpaRepository<Activity, Long> {
     List<Activity> findAllByTypeAndTeacher_Id(String type, Long teacherId);
 
     List<Activity> findAllByTypeAndSemesterAndSubjectAndClassGroup(String type, String semester, String subject, String classGroup);
-    
+
     List<Activity> findAllByTeacher(Teacher teacher);
 
+    List<Activity> findAllBySemesterAndTeacher(String semester, Teacher teacher);
+
+    List<Activity> findAllBySubjectAndTeacher(String subject, Teacher teacher);
+
+    List<Activity> findAllByStudentClassAndTeacher(String studentClass, Teacher teacher);
+
+    List<Activity> findAllByGroupAndTeacher(String group, Teacher teacher);
 }
