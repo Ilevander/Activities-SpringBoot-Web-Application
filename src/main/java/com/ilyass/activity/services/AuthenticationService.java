2b0c6aa4bebb9e1f74cca9d26228ef4bdb2117ca
dell <dell@DESKTOP-4P8U8AR>
2024-05-07 20:57:38 +0100

    Creating AuthentificationService

---
+++ src/main/java/com/ilyass/activity/services/AuthenticationService.java
@@ -0,0 +1,30 @@
+package com.ilyass.activity.services;
+
+import com.ilyass.activity.models.User;
+
+public interface AuthenticationService {
+
+    /**
+     * Authenticate a user based on username and password.
+     * 
+     * @param username The username of the user
+     * @param password The password of the user
+     * @return The authenticated user object if successful, null otherwise
+     */
+    User login(String username, String password);
+
+    /**
+     * Perform logout actions for the user.
+     * 
+     * @param user The user to logout
+     */
+    void logout(User user);
+
+    /**
+     * Hashes the provided password for storage in the database.
+     * 
+     * @param password The password to hash
+     * @return The hashed password
+     */
+    String hashPassword(String password);
+}
