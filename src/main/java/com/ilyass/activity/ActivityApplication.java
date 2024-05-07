cdd1fc3bf2166622d7668de100a2e3d1935f044a
dell <dell@DESKTOP-4P8U8AR>
2024-05-07 12:54:29 +0100

    @Bean configuration , adding @Builder to User model

--- src/main/java/com/ilyass/activity/ActivityApplication.java
+++ src/main/java/com/ilyass/activity/ActivityApplication.java
@@ -18,16 +18,33 @@ public class ActivityApplication {
 	/*
 	 * @Bean : Gives context to be lunched at the begining of the application , foeach method used by @Bean
 	 * start(UserRepository userRepository) As a DI like @Autowiered private StudentRepository studentRepository
+	 * Bellow we have many ways to create and instance object using Bean Object
 	 */
 	@Bean
 	public CommandLineRunner start(UserRepository userRepository) {
 		//Using lambda expression to avoid Java Verbosity Interface,inheritence,class...
 		return args ->{
-			User user = new User(); 
-			user.setFirstName(null);
-	        user.setLastName(null); 
-
-	        userRepository.save(user);
+			
+			User u1 = new User(); //NoArgsConstructor
+			u1.setFirstName("Ilyass");
+	        u1.setLastName("EL AMRI"); 
+	        u1.setUserName("ilyass07");
+	        u1.setPassword("123");
+	        userRepository.save(u1);
+	        
+	        User u2 = new User(null,"Yassine","Louis","yassne09",null,null); //AllArgsConstructor
+	        userRepository.save(u2);
+	        
+	        //Builder():is a design pattern given by lombok to instance object without need to constructor by specifiying which fields to build in object 
+	        User u3 = User.builder()
+	        		.firstName("toto")
+	        		.lastName("titi")
+	        		.build();
+	        userRepository.save(u3);
+	        
+	        
+	        
+	        
 		};
 	}
 }
