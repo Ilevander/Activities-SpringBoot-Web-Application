339003a4b98931b3bba7c45f90304ec50f0c84c1
dell <dell@DESKTOP-4P8U8AR>
2024-05-07 19:12:15 +0100

    Customizing Teacher Filter Service

--- src/main/java/com/ilyass/activity/services/TeacherFilterService.java
+++ src/main/java/com/ilyass/activity/services/TeacherFilterService.java
@@ -3,8 +3,13 @@ package com.ilyass.activity.services;
 import java.util.List;
 
 import com.ilyass.activity.models.Activity;
+import com.ilyass.activity.models.Teacher;
+
 public interface TeacherFilterService {
 
-    List<Activity> filterActivities(String semester, String subject, String classGroup);
+	List<Activity> filterActivitiesBySemester(String semester, Teacher teacher);
+    List<Activity> filterActivitiesBySubject(String subject, Teacher teacher);
+    List<Activity> filterActivitiesByClass(String studentClass, Teacher teacher);
+    List<Activity> filterActivitiesByGroup(String group, Teacher teacher);
 
 }
